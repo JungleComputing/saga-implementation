ace789873caae2fef42464f5036a2bf7851d668b
Ceriel Jacobs <c.j.h.jacobs@vu.nl>
2007-11-29 08:37:51 +0000

    Moved

---
+++ test/test/namespace/Sequence.java
@@ -0,0 +1,108 @@
+package test.namespace;
+
+import java.util.List;
+
+import org.ogf.saga.URL;
+import org.ogf.saga.buffer.Buffer;
+import org.ogf.saga.buffer.BufferFactory;
+import org.ogf.saga.file.File;
+import org.ogf.saga.file.FileFactory;
+import org.ogf.saga.namespace.Flags;
+import org.ogf.saga.namespace.NSDirectory;
+import org.ogf.saga.namespace.NSFactory;
+import org.ogf.saga.session.Session;
+import org.ogf.saga.session.SessionFactory;
+
+class Sequence {
+
+    /*
+     * Usage of Sequence
+     * 
+     * touch dirname/filename1 (create non-existing dirs on the fly) write
+     * message to filename1 test whether filename1 and filename2 exist filename1
+     * should exist, filename2 shouldn't move filename1 to filename2 and test
+     * again (filename1 doesn't exist, filename2 does) copy filename2 to
+     * filename1 and test again (both files exist) list the dir (both files
+     * should be displayed) filename2 removes itself list the dir (only
+     * filename1 should be visible) filename1 gets removed by dir
+     */
+
+    public static void main(String[] args) {
+        if (args.length != 4) {
+            System.err.println("Usage: run_saga_app test.namespace.Sequence filename1 filename2 dirname message");
+            System.exit(1);
+        }
+        String filename = args[0];
+        String newFilename = args[1];
+        String dir = args[2];
+        String message = args[3];
+              
+        try {
+            URL urlFile = new URL(filename);
+            URL urlNewFile = new URL(newFilename);
+            
+            Session session = SessionFactory.createSession();
+            File file = FileFactory.createFile(session, new URL(dir + "/" + filename),
+                    Flags.READWRITE.or(Flags.CREATE.or(Flags.CREATEPARENTS)));
+            System.out.println("File size (" + file.getURL().toString() + "): "
+                    + file.getSize() + " bytes (before write)");
+            Buffer buffer = BufferFactory.createBuffer();
+            buffer.setData(message.getBytes());
+            file.write(message.length(), buffer);
+            System.out.println("Written: '" + message + "' to "
+                    + file.getURL().toString());
+            System.out.println("File size (" + file.getURL().toString() + "): "
+                    + file.getSize() + " bytes (after write)");
+            System.out.println("File getCWD(): " + file.getCWD());
+            NSDirectory parentDir = NSFactory.createNSDirectory(session,
+                    file.getCWD(),
+                    Flags.CREATE.or(Flags.CREATEPARENTS));
+            System.out
+                    .println("before move: check for existence of both files");
+            System.out.println(" " + filename + ": "
+                    + parentDir.exists(urlFile));
+            System.out.println(" " + newFilename + ": "
+                    + parentDir.exists(urlNewFile));
+            
+            // NewFile considered relative to cwd of file.
+            file.move(urlNewFile, Flags.NONE.getValue());
+            System.out.println("moved '" + filename + "' to '" + newFilename
+                    + "'");
+            // old file still exists?
+            System.out.println("after move: check for existence of both files");
+            System.out.println(" " + filename + ": "
+                    + parentDir.exists(urlFile));
+            System.out.println(" " + newFilename + ": "
+                    + parentDir.exists(urlNewFile));
+            System.out.println("copied '" + newFilename + "' to '" + filename
+                    + "'");
+            
+            // Both urls considered relative to parentDir.
+            parentDir.copy(urlNewFile, urlFile, Flags.NONE.getValue());
+            System.out.println("after copy: check for existence of both files");
+            System.out.println(" " + filename + ": "
+                    + parentDir.exists(urlFile));
+            System.out.println(" " + newFilename + ": "
+                    + parentDir.exists(urlNewFile));
+            System.out.println("list of dir '" + dir + "'");
+            List<URL> urls = parentDir.list("", Flags.NONE.getValue());
+            URL[] urlsArray = urls.toArray(new URL[urls.size()]);
+            for (URL url : urlsArray) {
+                System.out.println(" " + url.toString());
+            }
+            file.remove(Flags.NONE.getValue());
+            System.out.println("removed '" + newFilename + "'");
+            System.out.println("list of dir '" + dir + "'");
+            urls = parentDir.list("", Flags.NONE.getValue());
+            urlsArray = urls.toArray(new URL[urls.size()]);
+            for (URL url : urlsArray) {
+                System.out.println(" " + url.toString());
+            }
+            System.out.println("removed '" + filename + "' from dir '" + dir
+                    + "'");
+            parentDir.remove(urlFile, Flags.NONE.getValue());
+        } catch (Throwable t) {
+            System.out.println("exception: " + t);
+        }
+    }
+}
---
+++ test/test/namespace/TaskCopyTest.java
@@ -0,0 +1,105 @@
+package test.namespace;
+
+import org.ogf.saga.URL;
+import org.ogf.saga.context.Context;
+import org.ogf.saga.context.ContextFactory;
+import org.ogf.saga.error.DoesNotExist;
+import org.ogf.saga.monitoring.Callback;
+import org.ogf.saga.monitoring.Metric;
+import org.ogf.saga.monitoring.Monitorable;
+import org.ogf.saga.namespace.Flags;
+import org.ogf.saga.namespace.NSDirectory;
+import org.ogf.saga.namespace.NSFactory;
+import org.ogf.saga.session.Session;
+import org.ogf.saga.session.SessionFactory;
+import org.ogf.saga.task.State;
+import org.ogf.saga.task.Task;
+import org.ogf.saga.task.TaskContainer;
+import org.ogf.saga.task.TaskFactory;
+import org.ogf.saga.task.TaskMode;
+import org.ogf.saga.task.WaitMode;
+
+/**
+ * A small test for TaskContainers (and copy()).
+ * It creates two tasks, one to copy an ftp file to a local file, and one
+ * to copy a http file to a local file. These tasks are stored in a
+ * task container, which has a callback installed on it, which just prints
+ * a message, demonstrating that it gets invoked.
+ * The task container is run and waited for. Any exceptions thrown by the tasks
+ * are printed.
+ */
+public class TaskCopyTest implements Callback {
+    
+    public boolean cb(Monitorable m, Metric metric, Context ctxt) {
+        try {
+            String value = metric.getAttribute(Metric.VALUE);
+            TaskContainer container = (TaskContainer) m;
+            try {
+                Task t = container.getTask(Integer.parseInt(value));
+                System.out.println("Metric " + metric.getAttribute(Metric.NAME)
+                        + " got triggered, task " + value + " now has state "
+                        + t.getState());
+            } catch(DoesNotExist e) {
+                System.out.println("Metric " + metric.getAttribute(Metric.NAME)
+                        + " got triggered, task " + value
+                        + " does not exist anymore?");
+            }
+        } catch(Throwable e) {
+            System.err.println("error" + e);
+            e.printStackTrace(System.err);
+        }
+        return true;
+    }
+    
+    private static TaskContainer createTasks(Session session) throws Exception {
+        NSDirectory dir = NSFactory.createNSDirectory(session, new URL("."));
+
+        // Create two copy tasks and a task container 
+        Task task1 = dir.copy(TaskMode.TASK,
+                new URL("ftp://ftp.cs.vu.nl/pub/ceriel/LLgen.tar.gz"),
+                new URL("file:///LLgen.tar.gz"), Flags.OVERWRITE.getValue());
+        Task task2 = dir.copy(TaskMode.TASK,
+                new URL("http://www.cs.vu.nl/ibis/index.html"),
+                new URL("file:///index.html"), Flags.OVERWRITE.getValue());
+        TaskContainer container = TaskFactory.createTaskContainer();
+        
+        // Install a callback detecting state changes in the tasks
+        container.addCallback(TaskContainer.TASKCONTAINER_STATE,
+                new TaskCopyTest());
+        
+        container.add(task2); 
+        container.add(task1);
+        return container;
+    }
+    
+    public static void main(String[] args) throws Exception {
+        Session session = null;
+        try {
+            session = SessionFactory.createSession(true);
+            
+            // Create an ftp context.
+            Context ftpContext = ContextFactory.createContext("ftp");
+            session.addContext(ftpContext);
+            
+            TaskContainer container = createTasks(session);
+            
+            container.run();
+            while (container.size() != 0) {
+                Task t = container.waitTasks(WaitMode.ANY);
+                System.out.println("Task finished, state = " + t.getState());
+                if (State.FAILED.equals(t.getState())) {
+                    try {
+                        t.rethrow();
+                    } catch(Throwable e) {
+                        System.err.println("Task threw exception: " + e);
+                        e.printStackTrace(System.err);
+                    }
+                }
+            }           
+        } finally {
+            // Explicit close is needed, unfortunately, for termination.
+            // This should be fixed.
+            session.close();
+        }
+    }
+}
