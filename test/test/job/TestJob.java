05c98443e3630d8d05a6b6a582237f7fa7c59699
Ceriel Jacobs <c.j.h.jacobs@vu.nl>
2008-01-23 13:19:20 +0000

    Updated test.

--- test/test/job/TestJob.java
+++ test/test/job/TestJob.java
@@ -14,14 +14,36 @@ public class TestJob implements Callback {
 
     public static void main(String[] args) {
         try {
+            // Make sure that the SAGA engine picks the javagat adaptor for JobService.
+            System.setProperty("JobService.adaptor.name", "javaGAT");
+            
+            // Make sure that javaGAT picks the gridsam adaptor
+            System.setProperty("GATPreferences.ResourceBroker.adaptor.name", "gridsam");
+
+            // Sandbox root must be an absolute path for the javaGAT gridsam adaptor
+            System.setProperty("GAT.sandbox.root", "/tmp");            
+
+            // Set a sandbox host for javaGAT, otherwise javagat thinks it is just localhost.
+            // This is only needed because the gridsam server is reached through a tunnel.
+            System.setProperty("GATPreferences.ResourceBroker.sandbox.host", "localhost:4567");
+
+            // Create the JobService. Note: the gridsam service is behind a firewall and is
+            // reached through an ssh tunnel, which the user must have set up beforehand.
+            JobService js = JobFactory.createJobService(new URL("https://localhost:18443/gridsam/services/gridsam"));
+
+            // Create a job description to execute "/bin/uname -a" on fs0.das3.cs.vu.nl.
+            // The output will be staged out to the current directory.
             JobDescription jd = JobFactory.createJobDescription();
             jd.setVectorAttribute(JobDescription.CANDIDATEHOSTS, new String[] {"fs0.das3.cs.vu.nl"});
             jd.setAttribute(JobDescription.EXECUTABLE, "/bin/uname");
             jd.setVectorAttribute(JobDescription.ARGUMENTS, new String[] { "-a" });
             jd.setAttribute(JobDescription.OUTPUT, "uname.out");
+            String host = java.net.InetAddress.getLocalHost().getCanonicalHostName();
+            String dir = System.getProperty("user.dir");
             jd.setVectorAttribute(JobDescription.FILETRANSFER,
-                    new String[] { "file:///home/ceriel/uname.out < uname.out" } );
-            JobService js = JobFactory.createJobService(new URL("https://localhost:18443/gridsam/services/gridsam"));
+                    new String[] { "file://" + host + dir + "/uname.out < uname.out" } );
+            
+            // Create the job, run it, and wait for it.
             Job job = js.createJob(jd);
             job.run();
             job.waitFor();
@@ -31,7 +53,8 @@ public class TestJob implements Callback {
             e.getCause().printStackTrace();
         }
     }
-    
+
+    // Callback monitors job.
     public boolean cb(Monitorable m, Metric metric, Context ctxt) {
         try {
             String value = metric.getAttribute(Metric.VALUE);
@@ -40,7 +63,7 @@ public class TestJob implements Callback {
             System.err.println("error" + e);
             e.printStackTrace(System.err);
         }
+        // Keep the callback.
         return true;
     }
-
 }
