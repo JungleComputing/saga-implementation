a1ff8810233462db97d9a21d07643e9ca794eaba
Ceriel Jacobs <c.j.h.jacobs@vu.nl>
2008-01-25 21:14:06 +0000

    Updated test, made independent of File (but dependent on FileOutputStream) which works better with JavaGAT

--- test/test/namespace/Sequence.java
+++ test/test/namespace/Sequence.java
@@ -2,13 +2,17 @@ package test.namespace;
 
 import java.util.List;
 
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+
 import org.ogf.saga.URL;
-import org.ogf.saga.buffer.Buffer;
-import org.ogf.saga.buffer.BufferFactory;
-import org.ogf.saga.file.File;
+import org.ogf.saga.context.Context;
+import org.ogf.saga.context.ContextFactory;
 import org.ogf.saga.file.FileFactory;
+import org.ogf.saga.file.FileOutputStream;
 import org.ogf.saga.namespace.Flags;
 import org.ogf.saga.namespace.NSDirectory;
+import org.ogf.saga.namespace.NSEntry;
 import org.ogf.saga.namespace.NSFactory;
 import org.ogf.saga.session.Session;
 import org.ogf.saga.session.SessionFactory;
@@ -26,64 +30,77 @@ class Sequence {
      * should be displayed) filename2 removes itself list the dir (only
      * filename1 should be visible) filename1 gets removed by dir
      */
-
+    
+    private static String getPassphrase() {
+        JPasswordField pwd = new JPasswordField();
+        Object[] message = { "grid-proxy-init\nPlease enter your passphrase.",
+                pwd };
+        JOptionPane.showMessageDialog(null, message, "Grid-Proxy-Init",
+                JOptionPane.QUESTION_MESSAGE);
+        return new String(pwd.getPassword());
+    }
+   
     public static void main(String[] args) {
         if (args.length != 4) {
             System.err.println("Usage: run_saga_app test.namespace.Sequence filename1 filename2 dirname message");
             System.exit(1);
         }
+        
         String filename = args[0];
         String newFilename = args[1];
         String dir = args[2];
         String message = args[3];
               
         try {
-            URL urlFile = new URL(filename);
-            URL urlNewFile = new URL(newFilename);
+            URL fileUrl = new URL(filename);
+            URL newFileUrl = new URL(newFilename);
+            URL completeFileUrl = new URL(dir + "/" + filename);
             
-            Session session = SessionFactory.createSession();
-            File file = FileFactory.createFile(session, new URL(dir + "/" + filename),
-                    Flags.READWRITE.or(Flags.CREATE.or(Flags.CREATEPARENTS)));
-            System.out.println("File size (" + file.getURL().toString() + "): "
-                    + file.getSize() + " bytes (before write)");
-            Buffer buffer = BufferFactory.createBuffer();
-            buffer.setData(message.getBytes());
-            file.write(buffer, message.length());
-            System.out.println("Written: '" + message + "' to "
-                    + file.getURL().toString());
-            System.out.println("File size (" + file.getURL().toString() + "): "
-                    + file.getSize() + " bytes (after write)");
-            System.out.println("File getCWD(): " + file.getCWD());
+            Session session = SessionFactory.createSession(true);
+            String scheme = completeFileUrl.getScheme();
+            if ("gsiftp".equals(scheme)) {
+                // Gridftp context.
+                Context context = ContextFactory.createContext("gridftp");
+                context.setAttribute(Context.USERPASS, getPassphrase());
+                session.addContext(context);
+            }
+
             NSDirectory parentDir = NSFactory.createNSDirectory(session,
-                    file.getCWD(),
-                    Flags.CREATE.or(Flags.CREATEPARENTS));
+                    new URL(dir), Flags.CREATE.or(Flags.CREATEPARENTS));
+            FileOutputStream stream = FileFactory.createFileOutputStream(session, completeFileUrl);
+            stream.write(message.getBytes());
+            stream.close();
+            System.out.println("Written: '" + message + "' to "
+                    + completeFileUrl);
+            NSEntry file = NSFactory.createNSEntry(session, completeFileUrl);
+            System.out.println("File getCWD(): " + file.getCWD());
             System.out
                     .println("before move: check for existence of both files");
             System.out.println(" " + filename + ": "
-                    + parentDir.exists(urlFile));
+                    + parentDir.exists(fileUrl));
             System.out.println(" " + newFilename + ": "
-                    + parentDir.exists(urlNewFile));
+                    + parentDir.exists(newFileUrl));
             
             // NewFile considered relative to cwd of file.
-            file.move(urlNewFile, Flags.NONE.getValue());
+            file.move(newFileUrl, Flags.NONE.getValue());
             System.out.println("moved '" + filename + "' to '" + newFilename
                     + "'");
             // old file still exists?
             System.out.println("after move: check for existence of both files");
             System.out.println(" " + filename + ": "
-                    + parentDir.exists(urlFile));
+                    + parentDir.exists(fileUrl));
             System.out.println(" " + newFilename + ": "
-                    + parentDir.exists(urlNewFile));
+                    + parentDir.exists(newFileUrl));
             System.out.println("copied '" + newFilename + "' to '" + filename
                     + "'");
             
             // Both urls considered relative to parentDir.
-            parentDir.copy(urlNewFile, urlFile, Flags.NONE.getValue());
+            parentDir.copy(newFileUrl, fileUrl, Flags.NONE.getValue());
             System.out.println("after copy: check for existence of both files");
             System.out.println(" " + filename + ": "
-                    + parentDir.exists(urlFile));
+                    + parentDir.exists(fileUrl));
             System.out.println(" " + newFilename + ": "
-                    + parentDir.exists(urlNewFile));
+                    + parentDir.exists(newFileUrl));
             System.out.println("list of dir '" + dir + "'");
             List<URL> urls = parentDir.list("", Flags.NONE.getValue());
             URL[] urlsArray = urls.toArray(new URL[urls.size()]);
@@ -100,9 +117,15 @@ class Sequence {
             }
             System.out.println("removed '" + filename + "' from dir '" + dir
                     + "'");
-            parentDir.remove(urlFile, Flags.NONE.getValue());
+            parentDir.remove(fileUrl, Flags.NONE.getValue());
         } catch (Throwable t) {
             System.out.println("exception: " + t);
+            t.printStackTrace(System.out);
+            Throwable e = t.getCause();
+            if (e != null) {
+                System.out.println("nested exception: " + e);
+                e.printStackTrace(System.out);
+            }
         }
     }
 }
