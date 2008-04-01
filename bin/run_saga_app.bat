175a59425f46ec38eff8e635ebdf8e77d961c3b1
Ceriel Jacobs <c.j.h.jacobs@vu.nl>
2008-04-01 15:13:30 +0000

    Updated: wrong classname

--- bin/run_saga_app.bat
+++ bin/run_saga_app.bat
@@ -1,14 +1,9 @@
 @echo off
 
-IF "%SAGA_LOCATION%X"=="X" set SAGA_LOCATION=.
-
-:: ---- do not touch anything below this line ----
-
-set SAGA_LIB_LOCATION=%SAGA_LOCATION%\lib
-set GAT_ADAPTOR_LOCATION=%SAGA_LIB_LOCATION%\adaptors\JavaGatAdaptor\adaptors
+IF "%SAGA_LOCATION%X"=="X" set SAGA_LOCATION=%~dp0..
 
 set SAGA_CLASSPATH=
 
-FOR %%i IN ("%SAGA_LIB_LOCATION%\*.jar") DO CALL "%SAGA_LOCATION%\bin\AddToClassPath.bat" %%i
+FOR %%i IN ("%SAGA_LOCATION%\lib\*.jar") DO CALL "%SAGA_LOCATION%\bin\AddToClassPath.bat" %%i
 
-java -cp "%CLASSPATH%";"%SAGA_CLASSPATH%" -Dlog4j.configuration=file:"%SAGA_LOCATION%"\log4j.properties -Dgat.adaptor.path="%GAT_ADAPTOR_LOCATION%" %*
+java -cp "%SAGA_CLASSPATH%";"%CLASSPATH%" -Dlog4j.configuration=file:"%SAGA_LOCATION%"\log4j.properties %*
