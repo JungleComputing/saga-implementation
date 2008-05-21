f83278e5b8541121b8a86c36c20b5b01b103253c
Ceriel Jacobs <c.j.h.jacobs@vu.nl>
2008-05-21 13:24:15 +0000

    Made to work on windows

--- bin/run_saga_app.bat
+++ bin/run_saga_app.bat
@@ -4,6 +4,6 @@ IF "%SAGA_LOCATION%X"=="X" set SAGA_LOCATION=%~dp0..
 
 set SAGA_CLASSPATH=
 
-FOR %%i IN ("%SAGA_LOCATION%\lib\*.jar") DO CALL "%SAGA_LOCATION%\bin\AddToClassPath.bat" %%i
+FOR %%i IN ("%SAGA_LOCATION%\lib\*.jar") DO CALL "%SAGA_LOCATION%\bin\AppendToClassPath.bat" %%i
 
 java -cp "%SAGA_CLASSPATH%";"%CLASSPATH%" -Dlog4j.configuration=file:"%SAGA_LOCATION%"\log4j.properties %*
