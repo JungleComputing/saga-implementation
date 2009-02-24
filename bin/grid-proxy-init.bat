cf8ec0a6160c7481e3eb67812b05f597f4669c60
Ceriel Jacobs <c.j.h.jacobs@vu.nl>
2009-02-24 13:56:38 +0000

    Made grid-proxy scripts to work with new gat

--- bin/grid-proxy-init.bat
+++ bin/grid-proxy-init.bat
@@ -4,4 +4,8 @@ IF "%JAVA_SAGA_LOCATION%X"=="X" set JAVA_SAGA_LOCATION=%SAGA_LOCATION%
 
 IF "%JAVA_SAGA_LOCATION%X"=="X" set JAVA_SAGA_LOCATION=%~dp0..
 
-java -DUID="%USERNAME%" -classpath "%JAVA_SAGA_LOCATION%\lib\adaptors\JavaGatAdaptor\adaptors\GlobusAdaptor\cog-jglobus.jar" org.globus.tools.ProxyInit %*
+set SAGA_CLASSPATH=
+
+FOR %%i IN ("%JAVA_SAGA_LOCATION%\lib\adaptors\JavaGatAdaptor\adaptors\shared\*.jar") DO CALL "%JAVA_SAGA_LOCATION%\bin\AppendToClassPath.bat" %%i
+
+java -DUID="%USERNAME%" -classpath "%JAVA_SAGA_LOCATION%\lib\adaptors\JavaGatAdaptor\adaptors\GlobusAdaptor\cog-jglobus.jar;%SAGA_CLASSPATH%" org.globus.tools.ProxyInit %*
