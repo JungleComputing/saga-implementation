93716e8bbbfbc3e8639396e0260d595ffff3e7f9
Emilian Miron <emilian.miron@gmail.com>
2009-10-12 19:02:11 +0000

    Prettier printing of arguments help spanning multiple lines in the saga shell.

--- apps/shell/src/org/ogf/saga/apps/shell/command/Help.java
+++ apps/shell/src/org/ogf/saga/apps/shell/command/Help.java
@@ -20,6 +20,30 @@ public class Help implements Command {
     public String getHelpExplanation() {
         return "print this help text";
     }
+    
+    private void printIndent(int len) {
+        for (; len > 0; --len) {
+            System.out.print(' ');
+        }
+    }
+    
+    /**
+     * For commands with multiple lines of arguments, try to print them nicely,
+     * with indentation such that all the lines have the same start column.
+     */
+    private void printHelpArguments(String name, String helpArguments) {
+        System.out.print(INDENT + name + INDENT);
+        int cmdArgsIndent = name.length() + 2;
+        
+        String[] lines = helpArguments.split("\n");
+        for (int i = 0; i < lines.length; ++i) {
+            if (i > 0) {
+                System.out.println();
+                printIndent(cmdArgsIndent);
+            }
+            System.out.print(lines[i]);
+        }
+    }
 
     public void execute(String[] args) {
         System.out.println("Available commands:");
@@ -27,17 +51,19 @@ public class Help implements Command {
         for (Map.Entry<String, Command> c : commands.entrySet()) {
             String name = c.getKey();
             Command cmd = c.getValue();
+            String helpArguments = cmd.getHelpArguments();
 
-            System.out.print(INDENT);
-
-            String cmdArg = name + " " + cmd.getHelpArguments();
-            System.out.print(cmdArg);
-
-            int cmdArgLen = cmdArg.length();
-            if (cmdArgLen > LEFT_COL_WIDTH) {
+            printHelpArguments(name, helpArguments);
+            int cmdArgLen = name.length() + helpArguments.length() + 2;
+            // If the command arguments help is only one line and we still have
+            // room, continue with command explanation on same line. 
+            if (cmdArgLen >= LEFT_COL_WIDTH ||
+                    cmd.getHelpArguments().contains("\n")) {
                 System.out.println();
-                System.out.print(INDENT);
                 cmdArgLen = 0;
+            } else {
+                System.out.print(INDENT);
+                ++cmdArgLen;
             }
 
             for (int i = 0; i < LEFT_COL_WIDTH - cmdArgLen; i++) {
