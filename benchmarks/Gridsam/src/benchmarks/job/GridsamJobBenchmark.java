19989b0a25a13b277d5da91eb7d619183cd0d9e3
Ceriel Jacobs <c.j.h.jacobs@vu.nl>
2010-01-07 10:59:44 +0000

    Generate jsdl instead of requiring user to supply it. Now, user supplies exec and args, like with the other job benchmarks.

--- benchmarks/Gridsam/src/benchmarks/job/GridsamJobBenchmark.java
+++ benchmarks/Gridsam/src/benchmarks/job/GridsamJobBenchmark.java
@@ -1,13 +1,22 @@
 package benchmarks.job;
 
-import java.io.File;
+import java.util.Arrays;
 import java.util.List;
 
+import org.apache.xmlbeans.XmlCursor;
 import org.icenigrid.gridsam.client.common.ClientSideJobManager;
 import org.icenigrid.gridsam.core.JobInstance;
 import org.icenigrid.gridsam.core.JobStage;
 import org.icenigrid.gridsam.core.JobState;
+import org.icenigrid.schema.jsdl.posix.y2005.m11.ArgumentType;
+import org.icenigrid.schema.jsdl.posix.y2005.m11.FileNameType;
+import org.icenigrid.schema.jsdl.posix.y2005.m11.POSIXApplicationDocument;
+import org.icenigrid.schema.jsdl.posix.y2005.m11.POSIXApplicationType;
+import org.icenigrid.schema.jsdl.y2005.m11.ApplicationType;
 import org.icenigrid.schema.jsdl.y2005.m11.JobDefinitionDocument;
+import org.icenigrid.schema.jsdl.y2005.m11.JobDefinitionType;
+import org.icenigrid.schema.jsdl.y2005.m11.JobDescriptionType;
+import org.icenigrid.schema.jsdl.y2005.m11.JobIdentificationType;
 
 import benchmarks.Benchmark;
 import benchmarks.BenchmarkRunner;
@@ -20,26 +29,61 @@ public class GridsamJobBenchmark implements Benchmark {
     private int firedEventCount;
     private Object savedState;
  
-    public GridsamJobBenchmark(String jsUrl, File exec) throws Exception {
-        jobDefinitionDocument = JobDefinitionDocument.Factory.parse(exec);
+    public GridsamJobBenchmark(String jsUrl, String exec, String[] args) throws Exception {
+        jobDefinitionDocument = generateJSDL(exec, args);
         jobManager = new ClientSideJobManager(new String[] { "-s", jsUrl },
                 ClientSideJobManager.getStandardOptions());
     }
+    
+    private JobDefinitionDocument generateJSDL(String exec, String[] args) {
+        JobDefinitionDocument jd = JobDefinitionDocument.Factory.newInstance();
+        JobDefinitionType jobDef = jobDefinitionDocument.addNewJobDefinition();
+        JobDescriptionType jobDescr = jobDef.addNewJobDescription();
+        JobIdentificationType jobid = jobDescr.addNewJobIdentification();
+        jobid.addJobProject("gridsam");
 
+        ApplicationType appl = jobDescr.addNewApplication();
+        XmlCursor cursor = appl.newCursor();
+        cursor.toEndToken();
+
+        POSIXApplicationDocument posixDoc = POSIXApplicationDocument.Factory
+                .newInstance();
+        POSIXApplicationType posixAppl = posixDoc.addNewPOSIXApplication();
+        FileNameType f = posixAppl.addNewExecutable();
+        f.setStringValue(exec);
+        for (String argument : args) {
+            ArgumentType arg = posixAppl.addNewArgument();
+            arg.setStringValue(argument);
+        } 
+
+        XmlCursor c = posixDoc.newCursor();
+        c.toStartDoc();
+        c.toNextToken();
+        c.moveXml(cursor);
+        
+        return jd;
+    }
+    
     public static void main(String[] args) {
         if (args.length < 3) {
             System.out.println("usage: java " + GridsamJobBenchmark.class.getName()
-                    + " <jobservice-url> <#runs> <JSDL file>");
+                    + " <jobservice-url> <#runs> <command> <arg>*");
             return;
         }
         
+        
         String jsUrl = args[0];
         int runs = Integer.parseInt(args[1]);
-        File exec = new File(args[2]);
+        String exec = args[2];
+        
+        String[] arguments = null;
+        if (args.length > 3) {
+            arguments = Arrays.copyOfRange(args, 3, args.length);
+        }
         
         Benchmark test;
         try {
-            test = new GridsamJobBenchmark(jsUrl, exec);
+            test = new GridsamJobBenchmark(jsUrl, exec, arguments);
         } catch (Throwable e) {
             e.printStackTrace();
             return;
