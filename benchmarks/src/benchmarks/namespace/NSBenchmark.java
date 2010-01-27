73357c6a1d95f50aa5245869b835cafaecffc57d
Mathijs den Burger <mdenburger@gmail.com>
2010-01-27 10:50:27 +0000

    removed unused constants

--- benchmarks/src/benchmarks/namespace/NSBenchmark.java
+++ benchmarks/src/benchmarks/namespace/NSBenchmark.java
@@ -2,8 +2,6 @@ package benchmarks.namespace;
 
 public interface NSBenchmark {
     
-    public static final int BIGFILE_SIZE = 100 * 1024 * 1024;   // bytes
-    public static final int DUMMY_DATA_SIZE = 1024 * 8;         // bytes
     public static final int DIR_COUNT = 10;       // <= 1000
     public static final int SUBDIR_COUNT = 10;    // per directory, <= 1000
     public static final int FILE_COUNT = 10;      // per subdirectory, <= 1000
