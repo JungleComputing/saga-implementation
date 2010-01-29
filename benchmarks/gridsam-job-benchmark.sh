6324b7f720bab2795ceb51e68a0b4e4fa32f7584
Mathijs den Burger <mdenburger@gmail.com>
2010-01-29 16:37:32 +0000

    removed Java 1.6-specific construct

--- benchmarks/gridsam-job-benchmark.sh
+++ benchmarks/gridsam-job-benchmark.sh
@@ -8,7 +8,22 @@ GRIDSAM_HOME="${OMIICLIENT}/gridsam"
 
 ENDORSED="-Djava.endorsed.dirs=${OMIICLIENT}/endorsed"
 
-exec java "${ENDORSED}" -classpath ".:${JAVA_SAGA_LOCATION}/lib/"'*'":${GRIDSAM_HOME}/lib/gridsam-client.jar:${OMIICLIENT}/conf" \
+# Jar-files from library.
+SAGA_CLASSPATH=""
+add_to_saga_classpath () {
+    JARFILES=`cd "$1" && ls *.jar 2>/dev/null`
+    for i in ${JARFILES} ; do
+	if [ -z "$SAGA_CLASSPATH" ] ; then
+	    SAGA_CLASSPATH="$1/$i"
+	else
+	    SAGA_CLASSPATH="$SAGA_CLASSPATH:$1/$i"
+	fi
+    done
+}
+
+add_to_saga_classpath $JAVA_SAGA_LOCATION/lib
+
+exec java "${ENDORSED}" -classpath ".:${SAGA_CLASSPATH}:${GRIDSAM_HOME}/lib/gridsam-client.jar:${OMIICLIENT}/conf" \
     -Dlog4j.configuration="file:${JAVA_SAGA_LOCATION}/log4j.properties" \
     -Daxis.ClientConfigFile="${OMIICLIENT}/conf/client-config.wsdd" \
     -Dgrid.config.dir="${OMIICLIENT}/conf" \
